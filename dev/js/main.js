8081edf556ea0f2348a24a0c9379c2a98e858748
barber <olegsmilez@gmail.com>
2021-02-02 20:08:33 +0200

    add #2 task

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,5 +1,5 @@
 "use strict";
-
+// 1
 let navItems = document.querySelectorAll('.nav-item');
 let tabContentItems = document.querySelectorAll('.tab-content__item')
 	
@@ -24,3 +24,35 @@ function setTabContentToActive(arr, id) {
 		}
 	});
 } 
+
+// 2
+
+function parseUlt(url) {
+	let strObj = {};
+	let preffixUrl = ["http://","https://","http://www.","https://www."];
+	let suffixHostName = ['.ua', '.ru', '.com'];
+	
+	preffixUrl.forEach(el => {
+		if (url.includes(el)) {
+			let tempUrl;
+			tempUrl = url.replace(el, '');
+			suffixHostName.forEach(el2 => {
+				if (tempUrl.includes(el2)) {
+					let tempArr = [];
+					tempArr = tempUrl.split(el2);
+					strObj.hostname = tempArr[0] + el2;
+					strObj.hash = tempArr[1].split('#')[1] ? '#' + tempArr[1].split('#')[1] : '';
+					if (tempUrl.includes('?')) {
+						strObj.pathname = tempArr[1].split('#')[0].split('?')[0] ? tempArr[1].split('#')[0].split('?')[0] : '';
+					} else {
+						strObj.pathname = tempArr[1].split('')[0] ? tempArr[1].split('#')[0] : '';
+					}
+				}
+			});
+		}
+	});
+	
+	return strObj;
+}
+let testUrl = parseUlt('http://ffwagency.com/about-ffw/your-digital-solutions-partner?a=1#test');
+console.log(testUrl)
